24f070f459120cdf25598fda646a1c819c3b3e20
Landon Wills <landonjwdev@gmail.com>
2024-06-25 22:26:14 -0400

    Add scroll to switch pc box

--- src/main/java/ca/landonjw/mixin/PCGUIMixin.java
+++ src/main/java/ca/landonjw/mixin/PCGUIMixin.java
@@ -2,15 +2,28 @@ package ca.landonjw.mixin;
 
 import ca.landonjw.GUIHandler;
 import com.cobblemon.mod.common.client.gui.pc.PCGUI;
+import com.cobblemon.mod.common.client.gui.pc.StorageWidget;
 import com.cobblemon.mod.common.client.keybind.CobblemonKeyBinds;
+import com.cobblemon.mod.common.client.storage.ClientPC;
 import com.cobblemon.mod.common.mixin.accessor.KeyBindingAccessor;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PCGUI.class)
-public class PCGUIMixin {
+public abstract class PCGUIMixin extends Screen {
+
+    @Shadow(remap = false) private StorageWidget storageWidget;
+    @Final @Shadow(remap = false) private ClientPC pc;
+
+    protected PCGUIMixin(Component component) {
+        super(component);
+    }
 
     @Inject(method = "keyPressed", at = @At("HEAD"))
     private void cobblemon_ui_tweaks$keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
@@ -18,6 +31,20 @@ public class PCGUIMixin {
         if (keyCode == summaryKey.boundKey().getValue()) {
             GUIHandler.INSTANCE.onSummaryPressFromPC((PCGUI)(Object)this);
         }
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double amount) {
+        var pastureWidget = storageWidget.getPastureWidget();
+        if (pastureWidget != null && pastureWidget.getPastureScrollList().isMouseOver(mouseX, mouseY)) {
+            pastureWidget.getPastureScrollList().mouseScrolled(mouseX, mouseY, amount);
+        }
+        else {
+            var newBox = (storageWidget.getBox() - (int)amount) % this.pc.getBoxes().size();
+            storageWidget.setBox(newBox);
+        }
+
+        return super.mouseScrolled(mouseX, mouseY, amount);
     }
 
 }
